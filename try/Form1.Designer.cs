c9e98c01358521efaf24245c4af468d11847ab8f
Eman288 <emantamam107@gmail.com>
2024-12-23 15:37:37 +0200

    pushing the scanner

---
+++ try/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace @try
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.code = new System.Windows.Forms.TextBox();
+            this.token = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // code
+            // 
+            this.code.Location = new System.Drawing.Point(523, 83);
+            this.code.Multiline = true;
+            this.code.Name = "code";
+            this.code.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.code.Size = new System.Drawing.Size(218, 246);
+            this.code.TabIndex = 1;
+            // 
+            // token
+            // 
+            this.token.Location = new System.Drawing.Point(12, 73);
+            this.token.Multiline = true;
+            this.token.Name = "token";
+            this.token.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.token.Size = new System.Drawing.Size(434, 329);
+            this.token.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.token);
+            this.Controls.Add(this.code);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox code;
+        private System.Windows.Forms.TextBox token;
+    }
+}
+
---
+++ try/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @try
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetToken(code.Text);
+        }
+
+        static List<(string, string)> MakeTokens(string line)
+        {
+            var tokenPatterns = new Dictionary<string, string>
+            {
+                { "KEYWORD", @"\b(اذا|طالما|متغير)\b" },
+                { "IDENT", @"[a-zA-Z\u0600-\u06FF][a-zA-Z0-9\u0600-\u06FF]*" },
+                { "NUM", @"\b\d+\b" },
+                { "OPERATOR", @"[+\-*/=<>!]+" },
+                { "ASSIGN", @"=" },
+                { "LPAREN", @"\(" },
+                { "RPAREN", @"\)" },
+                { "LBRACE", @"\{" },
+                { "RBRACE", @"\}" },
+                { "SEMICOLON", @";" },
+                { "WS", @"\s+" }
+            };
+
+
+            var tokenRegex = new Regex(string.Join("|", tokenPatterns.Select(kvp =>
+                $"(?<{kvp.Key}>{kvp.Value})"
+            )));
+
+            var tokens = new List<(string, string)>();
+
+            // Perform matching
+            var matches = tokenRegex.Matches(line);
+            foreach (Match match in matches)
+            {
+                foreach (var kvp in tokenPatterns.Keys)
+                {
+                    if (match.Groups[kvp].Success)
+                    {
+                        if (kvp != "WHITESPACE")  // Skip whitespace
+                        {
+                            tokens.Add((kvp, match.Value));
+                        }
+                        break;
+                    }
+                }
+            }
+
+            return tokens;
+        }
+
+
+        void GetToken(string s)
+        {
+            string c = "";
+            var tokens = MakeTokens(s);
+            foreach (var token in tokens)
+            {
+                c += $"({token.Item1}, \"{token.Item2}\"){Environment.NewLine}";
+            }
+            token.Text = c;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
